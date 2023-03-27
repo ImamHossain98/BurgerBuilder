d337fcf41de5c18d1fdde48f283aee8a4a84ccec
Imam Hossain <imamhossain98@gmail.com>
2023-03-27 13:39:39 +0600

    Burger modal updated

--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import Burger from './Burger/Burger';
 import Controls from './Controls/Controls';
+import Summary from './Summary/Summary';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap'
+
+const INGRIDIENT_PRICES = {
+    sasuage: 50,
+    cheese: 40,
+    meat: 90,
+    latus: 20
+}
 
 class BurgerBuilder extends Component {
 
@@ -10,15 +19,80 @@ class BurgerBuilder extends Component {
             { type: 'cheese', amount: 0 },
             { type: 'meat', amount: 0 },
             { type: 'latus', amount: 0 }
-        ]
+        ],
+        totalPrice: 80,
+        modalOpen: false,
+        purchasable: false,
+    }
+
+    updatePurchaseable = ingredients => {
+        const sum = ingredients.reduce((sum, element) => {
+            return sum + element.amount;
+        }, 0);
+        this.setState({ purchasable: sum > 0 })
+    }
+
+    addIngridientHandle = type => {
+        const newIngredients = [...this.state.ingredients];
+        const newPrice = this.state.totalPrice + INGRIDIENT_PRICES[type];
+        for (let item of newIngredients) {
+            if (item.type === type) item.amount++;
+        }
+        this.setState({ ingredients: newIngredients, totalPrice: newPrice })
+
+        this.updatePurchaseable(newIngredients)
+    }
+
+    removeIngridientHandle = type => {
+
+        const newIngredients = [...this.state.ingredients];
+        const newPrice = this.state.totalPrice - INGRIDIENT_PRICES[type];
+        for (let item of newIngredients) {
+            if (item.type === type) {
+                if (item.amount <= 0) return;
+                item.amount--;
+            }
+        }
+        this.setState({ ingredients: newIngredients, totalPrice: newPrice })
+
+        this.updatePurchaseable(newIngredients)
+    }
+
+    toggleModal = () => {
+        this.setState({
+            modalOpen: !this.state.modalOpen
+        })
     }
 
     render() {
         return (
-            <div className='d-flex flex-md-row flex-column'>
-                <Burger ingredients={this.state.ingredients} />
-                <Controls />
+            <div>
+                <div className='d-flex flex-md-row flex-column'>
+                    <Burger ingredients={this.state.ingredients} />
+                    <Controls
+                        ingriedientAdded={this.addIngridientHandle}
+                        ingredientRemoved={this.removeIngridientHandle}
+                        price={this.state.totalPrice}
+                        toggleModal={this.toggleModal}
+                        purchasable={this.state.purchasable}
+                    />
+                </div>
+
+                <Modal isOpen={this.state.modalOpen}>
+                    <ModalHeader>Your order summary</ModalHeader>
+                    <ModalBody>
+                        <h5>Total Price: {this.state.totalPrice.toFixed(0)} BDT</h5>
+                        <Summary ingredients={this.state.ingredients} />
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color='success' onClick={this.toggleModal}>Continue to checkout</Button>
+                        <Button color='secondary' onClick={this.toggleModal}>Cancel</Button>
+                    </ModalFooter>
+
+                </Modal>
+
             </div>
+
         );
     }
 }
--- src/components/BurgerBuilder/Controls/Controls.js
+++ src/components/BurgerBuilder/Controls/Controls.js
@@ -4,23 +4,26 @@ import { Card, CardBody, CardFooter, CardHeader, Button } from 'reactstrap'
 const controls = [
     { label: 'Latus', type: "latus" },
     { label: 'Cheese', type: "cheese" },
-    { label: 'Meat', type: " meat " },
-    { label: 'Sasuage', type: " sasuage " }
+    { label: 'Meat', type: "meat" },
+    { label: 'Sasuage', type: "sasuage" }
 ]
 
 const BuildControl = props => {
     return (
-        < div className="d-flex">
+        < div className="d-flex justify-content-between">
             <div className="mr-auto ml-5" style={{ fontWeight: 'bold', fontSize: "1.2rem" }}>{props.label}</div>
-            <button className='btn btn-danger btn-sm m-1'>less</button>
-            <button className='btn btn-success btn-sm m-1'>More</button>
+            <div>
+                <button className='btn btn-danger btn-sm m-1' onClick={props.removed}>less</button>
+                <button className='btn btn-success btn-sm m-1' onClick={props.added}>More</button>
+            </div>
+
         </div >
     )
 
 
 }
 
-const Controls = () => {
+const Controls = props => {
     return (
         <div className='container ml-md-5' style={{ textAlign: "center" }}>
             <Card style={{
@@ -39,13 +42,16 @@ const Controls = () => {
                                 label={item.label}
                                 type={item.type}
                                 key={Math.random()}
+                                added={() => props.ingriedientAdded(item.type)}
+                                removed={() => props.ingredientRemoved(item.type)}
 
 
                             />
                         })
                     }
                 </CardBody>
-                <CardFooter><h5>Price: BDT</h5></CardFooter>
+                <CardFooter><h5>Price:<strong>{props.price}</strong>BDT</h5></CardFooter>
+                <Button disabled={!props.purchasable} onClick={props.toggleModal}>Order Now</Button>
             </Card>
 
         </div>
